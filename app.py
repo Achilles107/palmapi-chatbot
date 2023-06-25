f526a9537fb499723703a47a26ae516d1d33058e
Abhinav Shukla <abhinavshukla835@gmail.com>
2023-06-25 00:45:11 +0000

    Integrated Palm Api

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+import google.generativeai as palm
 
 app = Flask(__name__)
 messages = []
+
+PALM_KEY = 'AIzaSyBg_mbXxMU-lnYjJ9ow29QZc4cEx2dju7s'
+palm.configure(api_key=PALM_KEY)
 
 @app.route('/', methods=['GET', 'POST'])
 def chat():
@@ -9,8 +13,9 @@ def chat():
         input_text = request.form.get('input_text')
         user_message = {'sender': 'You', 'content': input_text}
         messages.append(user_message)
-
-        reply_message = {'sender': 'ChatBot', 'content': input_text + 'Z'}
+        
+        reply = palm.chat(context="Be a Cowboy", messages=input_text)
+        reply_message = {'sender': 'ChatBot', 'content': reply.last}
         messages.append(reply_message)
 
     return render_template('index.html', messages=messages)
