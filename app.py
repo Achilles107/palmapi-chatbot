093d31e58382b149f6719b9a00a0881530fc16b0
Abhinav Shukla <abhinavshukla835@gmail.com>
2023-06-25 00:29:40 +0000

    Chatting Interface

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+messages = []
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def chat():
     if request.method == 'POST':
         input_text = request.form.get('input_text')
-        modified_text = input_text + 'Z'
-        return render_template('index.html', modified_text=modified_text)
-    return render_template('index.html')
+        user_message = {'sender': 'You', 'content': input_text}
+        messages.append(user_message)
+
+        reply_message = {'sender': 'ChatBot', 'content': input_text + 'Z'}
+        messages.append(reply_message)
+
+    return render_template('index.html', messages=messages)
 
 if __name__ == '__main__':
     app.run()
