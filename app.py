7d153b68ed715c22d15f8833e492a70d6810b826
Abhinav Shukla <abhinavshukla835@gmail.com>
2023-06-30 01:52:54 +0000

    Added Config files

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
 import google.generativeai as palm
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 messages = []
 
-PALM_KEY = 'YOUR_KEY'
+load_dotenv()
+PALM_KEY = os.getenv('PALM_KEY')
 palm.configure(api_key=PALM_KEY)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,8 +17,8 @@ def chat():
         user_message = {'sender': 'You', 'content': input_text}
         messages.append(user_message)
         
-        reply = palm.chat(context="Be a Cowboy", messages=input_text)
-        reply_message = {'sender': 'ChatBot', 'content': reply.last}
+        reply = palm.chat(context="Behave like a royal Princess. And If someone asks your name, say 'Princess Alice: Mother of dragons'", messages=input_text)
+        reply_message = {'sender': 'Princess Alice', 'content': reply.last}
         messages.append(reply_message)
 
     return render_template('index.html', messages=messages)
